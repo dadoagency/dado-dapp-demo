d672dc97bae24257b9137a67a47953870567d5b9
Max Tyrrell <maxtyrrell0@gmail.com>
2021-06-25 11:00:57 +0200

    feat(frontend): setup simple React app with Snowpack

---
+++ frontend/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/* Add JavaScript code here! */
+console.log("Hello World! You did it! Welcome to Snowpack :D");
+
+ReactDOM.render(
+  <EthereumProvider>
+    <App />
+  </EthereumProvider>,
+  document.getElementById("root")
+);
