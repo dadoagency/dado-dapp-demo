e93db381de83cc4f77df551401392ad62c49f373
Max Tyrrell <maxtyrrell0@gmail.com>
2021-06-30 12:04:47 +0200

    feat(frontend): add buttons to storeValue and retrieveValue

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,23 +1,83 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-import { BoxContractProvider, useBoxContract } from './providers/BoxContractProvider'
-
-/* Add JavaScript code here! */
-console.log("Hello World! You did it! Welcome to Snowpack :D");
+import {
+  BoxContractProvider,
+  useBoxContract,
+} from "./providers/BoxContractProvider";
 
 function App() {
-  const [value, setValue] = useState()
-  const { retrieveValue } = useBoxContract()
+  const [fetchedValue, setFetchedValue] = useState();
+  const { retrieveValue, connectWallet, signer, chainId } = useBoxContract();
 
   async function onRetrieveValue() {
     const res = await retrieveValue();
-    setValue(res);
+    console.log("res!", res);
+    setFetchedValue(res.toString());
   }
 
-  return (<div>
-    <button onClick={onRetrieveValue}>Retrieve value</button> (value is here:) {value}
-  </div>)
+  return (
+    <div>
+      <div>
+        <code>Connected to chainId {chainId}</code>
+      </div>
+      <hr />
+      <ConnectWallet />
+      <hr />
+      <button onClick={onRetrieveValue}>Retrieve fetchedValue</button>{" "}
+      (fetchedValue is here:) {fetchedValue}
+      <img src={fetchedValue} />
+      <StoreValueForm />
+    </div>
+  );
+}
+
+function StoreValueForm() {
+  const { storeValue } = useBoxContract();
+  const [newValue, setNewValue] = useState("");
+
+  function onNewValueFormSubmit(e) {
+    e.preventDefault();
+    console.log("e.target", e.target);
+
+    console.log("about to invoke storeValue!");
+    const res = storeValue(newValue);
+  }
+
+  function onNewValueChange(e) {
+    const enteredValue = e.target.value;
+    setNewValue(enteredValue);
+  }
+
+  return (
+    <form onSubmit={onNewValueFormSubmit}>
+      <input
+        name="newValue"
+        type="number"
+        value={newValue}
+        onChange={onNewValueChange}
+      />
+      <input type="submit" value="Submit transaction" />
+    </form>
+  );
+}
+
+function ConnectWallet() {
+  const { connectWallet, signer } = useBoxContract();
+
+  async function onConnectWalletClick() {
+    await connectWallet();
+    console.log("signer is", signer);
+  }
+
+  if (signer)
+    return (
+      <p>
+        Connected with: <code>{signer.provider.provider.selectedAddress}</code>
+      </p>
+    );
+
+  return <button onClick={onConnectWalletClick}>Connect Wallet</button>;
 }
 
 ReactDOM.render(
@@ -29,6 +89,7 @@ ReactDOM.render(
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
-if (undefined /* [snowpack] import.meta.hot */ ) {
-  undefined /* [snowpack] import.meta.hot */ .accept();
+if (undefined /* [snowpack] import.meta.hot */) {
+  undefined /* [snowpack] import.meta.hot */
+    .accept();
 }
